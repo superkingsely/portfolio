1c754cb8addc621e74e66e78338c65899bc2c7ed
chijioke <superkingsely@gmail.com>
2024-03-01 01:37:24 -0800

    scrollreal,typed and css anime added

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ handburger.addEventListener('click',()=>{
         img.setAttribute('src','menu-line.png')
     }
 })
+mobileMenu.addEventListener('click',()=>{
+    mobileMenu.classList.remove('show')
+    img.setAttribute('src','menu-line.png')
+
+
+})
+
+// no customization
+
+ScrollReveal({
+    reset:true,
+    distance:'100px',
+    duration:3000,
+    // delay:2000
+});
+
+// nw to make use of the scroll
+
+ScrollReveal().reveal('.home-content, .about-content',{origin:'left'});
+ScrollReveal().reveal('.home-img, .about-img',{origin:'right'});
+
+// ScrollReveal().reveal('.home-content', { duration: 2000, origin: 'top', distance: '100px' });
+
+// typed js
+const typed=new Typed('.niche',{
+    strings:['Frontend-developer','Backend-developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
